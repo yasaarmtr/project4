bf70a1e751f45b82a7d12523e50a8ce964871859
Yasaar Mauthoor <91458263+yasaarmtr@users.noreply.github.com>
2024-06-27 11:25:34 +0000

    first commit

---
+++ main/views.py
@@ -0,0 +1,23 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
+
+# # Create your views here.
+
+# def main(request):
+#     template = loader.get_template('template/myfirst.html')
+#     return HttpResponse(template.render())
+# ------
+# from django.http import HttpResponse
+# from django.template import loader
+
+# def members(request):
+#   template = loader.get_template('myfirst.html')
+#   return HttpResponse(template.render())
+
+from django.http import HttpResponse
+from django.template import loader
+
+def main(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
